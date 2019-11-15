956cff2188bf22e4f969de05c05f0ca156cb14dd
Vocho <vocho17@gmail.com>
2019-11-15 01:24:34 -0600

    Challenge complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,50 @@
 import './styles.css'
 
+const autoValues = {
+  terms: [24, 36, 48],
+  minLoan: 3000,
+  maxLoan: 100000
+}
+
+const homeValue = {
+  terms: [120, 180, 240],
+  minLoan: 30000,
+  maxLoan: 4500000
+}
+
+export const changeGarantia = garantiaSelect => {
+  if (garantiaSelect === 'veiculo') {
+    fillSelectTerms(autoValues.terms)
+    fillSliderValues(autoValues.minLoan, autoValues.maxLoan)
+  } else {
+    fillSelectTerms(homeValue.terms)
+    fillSliderValues(homeValue.minLoan, homeValue.maxLoan)
+  }
+}
+
+export const fillSelectTerms = terms => {
+  const selectParcelas = document.getElementById('parcelas')
+  selectParcelas.options.length = 0
+  terms.forEach(term => {
+    const option = document.createElement("option")
+    option.text = term
+    option.value = term
+    selectParcelas.add(option)
+  })
+}
+
+export const fillSliderValues = (min, max) => {
+  const textSlider = document.getElementById('valor-emprestimo')
+  const slider = document.getElementById('valor-emprestimo-range')
+  const labelContainer = slider.parentNode.children[1]
+  textSlider.value = parseInt(max / 2)
+  labelContainer.children[0].textContent = min
+  labelContainer.children[1].textContent = max
+  slider.min = min
+  slider.max = max
+  slider.value = parseInt(max / 2)
+}
+
 export const checkFormValidity = formElement => formElement.checkValidity()
 
 export const getFormValues = formElement =>
@@ -11,16 +56,10 @@ export const getFormValues = formElement =>
     }))
 
 export const toStringFormValues = values => {
-  const match = matchString => value => value.field === matchString
-  const IOF = 6.38 / 100
-  const INTEREST_RATE = 2.34 / 100
-  const TIME = values.find(match('parcelas')).value / 1000
-  const VEHICLE_LOAN_AMOUNT = values.find(match('valor-emprestimo')).value
-
   return `Confirmação\n${values
     .map(value => `Campo: ${value.field}, Valor: ${value.value}`)
     .join('\n')}`.concat(
-      `\nTotal ${(IOF + INTEREST_RATE + TIME + 1) * VEHICLE_LOAN_AMOUNT}`
+      `\nTotal ${calculateTotalLoan()}`
     )
 }
 
@@ -45,44 +84,71 @@ export function Submit(formElement) {
   })
 }
 
-export function handleChangeRangeVehicleUnderWarranty(
-  warrantyRangeElement,
-  vehicleWarrantyElement
-) {
-  const MIN_VALUE = 12000.0
-  warrantyRangeElement.addEventListener('change', function (event) {
-    vehicleWarrantyElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
+export const updateTotalLabel = () => {
+  const quota = document.getElementsByClassName('quota__container--price')
+  const amount = document.getElementsByClassName('amount__container--price')
+  const tax = document.getElementsByClassName('tax__container--price')
+  quota[0].textContent = calculateTotalLoan()
+  amount[0].textContent = `R$ ${(calculateTotalLoan() / Number(document.getElementById('parcelas').value)).toFixed(2)}`
+  tax[0].textContent = `R$ ${(calculateTotalLoan() * 0.10).toFixed(2)} %`
+  console.log(`R$ ${calculateTotalLoan()}`)
+}
+
+export const handleChangeSlide = (rangeElement, warrantyElement) => {
+  rangeElement.addEventListener('change', function (event) {
+    warrantyElement.value = event.target.value
+    updateTotalLabel()
   })
 }
 
-export function handleChangeVehicleLoanAmount(
-  loanAmountRangeElement,
-  loanAmountElement
-) {
-  const MIN_VALUE = 30000.0
-  loanAmountRangeElement.addEventListener('change', function (event) {
-    loanAmountElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
+export const handleChangeGarantia = garantiaElement => {
+  garantiaElement.addEventListener('change', function () {
+    changeGarantia(garantiaElement.value)
+    updateTotalLabel()
   })
+}
+
+export const handleChangeParcelas = parcelasElement => {
+  parcelasElement.addEventListener('change', function () {
+    updateTotalLabel()
+  })
+}
+
+export const calculateTotalLoan = () => {
+  const IOF = 6.38 / 100
+  const INTEREST_RATE = 2.34 / 100
+  const TIME = Number(document.getElementById('parcelas').value) / 1000
+  const LOAN_AMOUNT = Number(document.getElementById('valor-emprestimo').value)
+  const TOTAL = (IOF + INTEREST_RATE + TIME + 1) * LOAN_AMOUNT
+  return TOTAL.toFixed(2)
 }
 
 export default class CreditasChallenge {
-  static initialize() {
+  static initialize () {
     this.registerEvents()
+    fillSliderValues(autoValues.minLoan, autoValues.maxLoan)
+    updateTotalLabel()
   }
 
-  static registerEvents() {
+  static registerEvents () {
     Submit(document.querySelector('.form'))
 
-    handleChangeRangeVehicleUnderWarranty(
+    handleChangeSlide(
       document.getElementById('valor-garantia-range'),
       document.getElementById('valor-garantia')
     )
 
-    handleChangeVehicleLoanAmount(
+    handleChangeSlide(
       document.getElementById('valor-emprestimo-range'),
       document.getElementById('valor-emprestimo')
+    )
+
+    handleChangeGarantia(
+      document.getElementById('garantia')
+    )
+
+    handleChangeParcelas(
+      document.getElementById('parcelas')
     )
   }
 }
